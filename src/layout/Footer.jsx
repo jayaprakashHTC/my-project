898701ae5bc7fb3546f300ebbe77f8e30a2ffc2e
jayaprakashHTC <jpp177.galla@gmail.com>
2023-09-05 18:26:32 +0530

    ux and ui header and footer com created

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterCom = () => {
+  return (
+    <footer className='bg-[#000] p-[30px]'>
+        <p className='text-[#fff] text-center'>@Copy Right 2023</p>
+    </footer>
+  )
+}
+
+export default FooterCom
---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      width:"1200px",
+      padding:"16px"
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Caveat', 'cursive'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      'xsm':{ 'max': '640px'},
+      //@media small mobile devices
+
+      'sm': {'min': '641px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      }
+    },
+  },
+  plugins: [],
+}
+
